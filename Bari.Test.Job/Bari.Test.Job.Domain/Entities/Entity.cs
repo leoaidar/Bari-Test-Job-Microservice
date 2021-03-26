c1cc923040eb2f0099dfc708ae2f236a16eaecb3
Leonardo Aidar <leonardo.silva@wooza.com.br>
2021-03-26 02:57:16 -0300

    Added Domain layer with the Models

---
+++ Bari.Test.Job/Bari.Test.Job.Domain/Entities/Entity.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Bari.Test.Job.Domain.Entities
+{
+    public abstract class Entity : IEquatable<Entity>
+    {
+        public Entity()
+        {
+            Id = Guid.NewGuid();
+            CreateDate = LastUpdateDate = DateTime.Now;
+        }
+
+        public Guid Id { get; private set; }
+        public DateTime? CreateDate { get; private set; }
+        public DateTime? LastUpdateDate { get; private set; }
+        public bool Invalid { get; set; }
+
+        public abstract void Validate();
+
+        public bool Equals(Entity other)
+        {
+            return Id == other.Id;
+        }
+        public void EntityModified()
+        {
+            LastUpdateDate = DateTime.Now;
+        }
+    }
+}
---
+++ Bari.Test.Job/Bari.Test.Job.Domain/Entities/Message.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Bari.Test.Job.Domain.Entities
+{
+    public class Message : Entity
+    {
+        public string Body { get; set; }
+
+        public double Timestamp { get { return (double)((TimeSpan)(CreateDate - new DateTime(1970, 1, 1, 0, 0, 0, 0).ToLocalTime())).TotalSeconds; } }
+
+        public override void Validate()
+        {
+            if (string.IsNullOrEmpty(Body?.Trim()))
+                Invalid = true;
+        }
+
+        
+    }
+}
